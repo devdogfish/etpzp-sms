69048d0142ec3dff443c0cbace24049a711b7041
Mac Pro <mac@null.computer>
2024-12-12 21:49:30 +0000

    [NEW] Connect database sucessfully, create db() helper function for easy queries.

--- app/[locale]/(main)/(no-middlebar)/send/page.tsx
+++ app/[locale]/(main)/(no-middlebar)/send/page.tsx
@@ -1,7 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { sendMessage } from "@/lib/actions/fetch-message-working";
+import { testFetch } from "@/lib/send-sms/data";
 
-export default function Page() {
+export default async function Page() {
+  const data = await testFetch();
+  console.log(data);
   return (
     <div className="p-3 flex flex-col">
       <h2>Send a sms</h2>
---
+++ lib/send-sms/data.ts
@@ -0,0 +1,6 @@
+import db from "@/lib/db";
+
+export async function testFetch() {
+  const result = await db("SELECT NOW()");
+  return result.rows;
+}
